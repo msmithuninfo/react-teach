a5129c3471be56b4593a6bfbda55cd6277a79f5e
Mithun <mithuncseseu@gmail.com>
2021-03-01 10:38:14 +0600

    first commit

---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FunctionalComponent(){
+    return (
+        <div className="funcom">
+            <h1>I am Functional Component</h1>
+        </div>
+    );
+}
+
+export default FunctionalComponent;
